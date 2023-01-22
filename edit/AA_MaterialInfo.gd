73f5e060241354c17e095b832edfed38534fdf32
j v <vantjac@gmail.com>
2023-01-22 14:08:27 -0800

    Resource for built in materials

---
+++ edit/AA_MaterialInfo.gd
@@ -0,0 +1,6 @@
+
+class_name AA_MaterialInfo extends Resource  # move it to the top of the very long resource menu
+
+export(String) var guid = ""
+export(Texture) var texture
+export(Vector2) var texture_scale = Vector2(1, 1)
